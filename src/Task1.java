80dcafa565ac2564064aed107da7d8b9b993fa1f
Vlad Romanovich <3301889v@gmail.com>
2022-03-20 12:20:24 +0300

    Home worke2

--- src/Task1.java
+++ src/Task1.java
@@ -1,15 +1,21 @@
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import java.net.SocketOption;
+
 public class Task1 {
     // Вычислить факториал целых чисел от 0 до 10 при помощи while
     public static void main(String[] args) {
-        int x = 10;
+        int x = 2;
         int i = 1;
         int r = 1;
+        if(x==0){
+            System.out.println(r);
+        }else {
         while (i <= x) {
             r = r * i;
             i++;
 
             System.out.println(r);
-
+        }
         }
 
 
