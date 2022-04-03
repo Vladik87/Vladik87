4b8292a5ac8036d205ac0f3371bd9f7dec6f71e0
Vlad Romanovich <3301889v@gmail.com>
2022-04-03 22:08:23 +0300

    Home_work6 задание 3

---
+++ src/Home_work6/Boss.java
@@ -0,0 +1,14 @@
+package Home_work6;
+
+public class Boss extends engineer{
+    @Override
+    public void salary() {
+        int a = 0; //оклад
+        double salary = 0;
+        salary = a + a * 5;
+        Boss Koly = new Boss();
+
+
+    }
+
+}
---
+++ src/Home_work6/worker.java
@@ -0,0 +1,17 @@
+package Home_work6;
+
+public class worker {
+    public void salary(){
+        int h=0; //часы
+        int salary = h*15;//15-Ставка в час
+    }
+    worker Oleg = new worker();
+
+    public worker getOleg() {
+        return Oleg;
+    }
+
+    public void setOleg(worker oleg) {
+        Oleg = oleg;
+    }
+}
